dff1306fbeebce1a7f4ce88ec7b5ca98e65554f7
pro2s <protasov.s.s@gmail.com>
2018-07-06 15:39:43 +0300

    add migration, node postinstall to deploy on heroku

---
+++ database/migrations/2018_06_05_000000_create_quiz_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuizTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quiz', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('ended_at')->nullable();
+            $table->boolean('active');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quiz');
+    }
+}
