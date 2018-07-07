f6aafe99fa85426dfe62712e34c8b53f5623271d
pro2s <protasov.s.s@gmail.com>
2018-07-07 13:43:41 +0300

    another https fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -12,10 +11,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
-        if(env('REDIRECT_HTTPS')) {
-            $url->forceScheme('https');
+        if(App::environment('production')) {
+            URL::forceScheme('https');
         }
     }
 
