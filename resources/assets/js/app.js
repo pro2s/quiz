b6f56307e317a8742bf1131694352ea4ff3f97e7
pro2s <dev@pro2s.ru>
2019-09-11 08:06:14 +0300

    Fix security and build

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,7 +8,6 @@ require('./bootstrap');
 import Vue from 'vue';
 
 import Notifications from 'vue-notification';
-import ActionQuizRow from './components/dashboard/ActionQuizRow.vue';
 import ActiveButton from './components/dashboard/ActiveButton.vue';
 import DeleteButton from './components/dashboard/DeleteButton.vue';
 import QuestionsList from './components/dashboard/QuestionsList.vue';
@@ -18,10 +17,6 @@ import * as ModalDialogs from 'vue-modal-dialogs';
 Vue.use(ModalDialogs);
 Vue.use(Notifications);
 
-import ActiveButton from './components/ActiveButton.vue';
-import DeleteButton from './components/DeleteButton.vue';
-import ActionRow from './components/ActionRow.vue';
-import QuestionsList from './components/QuestionsList.vue';
 window.Vue = Vue;
 /**
  * Next, we will create a fresh Vue application instance and attach it to
