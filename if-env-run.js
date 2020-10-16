0cc9935fc9e3bdbfc3e0481cecb3b7003fd023d9
Sergey Protasov <protasov.s.s@gmail.com>
2020-10-17 00:47:14 +0300

    Fix if script

--- if-env-run.js
+++ if-env-run.js
@@ -1,9 +1,9 @@
-#!/usr/bin/env node 
+#!/usr/bin/env node
 
 const {spawn} = require('child_process');
 
-const argv = process.argv
-const key = argv[2]
-const command = argv[3]
+const [,,key = '', command = ''] = process.argv;
 
-if (key in process.env) spawn('npm', ['run', command], { stdio: 'inherit' });
+if (key in process.env && command.length) {
+    spawn('npm', ['run', command], { stdio: 'inherit', shell: true });
+}
